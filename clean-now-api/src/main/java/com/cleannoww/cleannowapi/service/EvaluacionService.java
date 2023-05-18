da9283c3f412c6a0f0e7b5790b760ca5d58ddd4e
Marcell1n1 <hfarfana1@upao.edu.pe>
2023-05-18 17:18:45 -0500

    feat: Creacion de Evaluacion Servicio, Repository, ServiceImpl, EvaluacionController

---
+++ clean-now-api/src/main/java/com/cleannoww/cleannowapi/service/EvaluacionService.java
@@ -0,0 +1,7 @@
+package com.cleannoww.cleannowapi.service;
+
+import com.cleannoww.cleannowapi.domain.Evaluacion;
+
+public interface EvaluacionService {
+    Evaluacion create(Evaluacion evaluacion);
+}
---
+++ clean-now-api/src/main/java/com/cleannoww/cleannowapi/service/EvaluacionServiceImpl.java
@@ -0,0 +1,12 @@
+package com.cleannoww.cleannowapi.service;
+
+import com.cleannoww.cleannowapi.domain.Evaluacion;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EvaluacionServiceImpl implements EvaluacionService{
+    @Override
+    public Evaluacion create(Evaluacion evaluacion) {
+        return null;
+    }
+}
